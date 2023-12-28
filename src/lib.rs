2479770f660bb864b44c4662c220539a2f2dc5cb
Lydia Samuel <20437872+lydiasamuel@users.noreply.github.com>
2023-12-28 13:49:44 +0000

    Implemented command line functionality

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,214 @@ pub mod nfa;
 pub mod automata;
 pub mod regex;
 pub mod dfa;
+
+use std::{fs, error::Error, env};
+
+use automata::AutomataState;
+use dfa::build_dfa;
+use graph::{NodeIndex, Graph};
+use nfa::build_nfa;
+use regex::get_alphabet;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+pub fn run(config: Config)  -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = search(&config.query, &contents, config.ignore_case);
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str, ignore_case: bool) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    let postfix_regex = postfixer::transform(query.to_string()).unwrap();
+
+    let alphabet = get_alphabet(&postfix_regex);
+    
+    let (handle, nfa) = build_nfa(postfix_regex);
+
+    let (start, dfa) = build_dfa(handle, nfa, alphabet);
+
+    for line in contents.lines() {
+        if check_line_matches(start, &dfa, line, ignore_case) {
+            results.push(line);
+        }
+    }
+
+    return results
+}
+
+
+fn check_line_matches(start_of_dfa: NodeIndex, dfa: &Graph<AutomataState, char>, line: &str, ignore_case: bool) -> bool {
+    for i in 0..line.len() {
+        let sub_line = &line[i..];
+
+        let automata_has_accepted = run_automata(start_of_dfa, dfa, sub_line, ignore_case);
+        
+        if automata_has_accepted {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn run_automata(start_of_dfa: NodeIndex, dfa: &Graph<AutomataState, char>, sub_line: &str, ignore_case: bool) -> bool {
+    let mut current_node = start_of_dfa;
+
+    for c in sub_line.chars() {
+        let mut can_progress = false;
+        let outgoing_edges = dfa.outgoing_edges(current_node).unwrap();
+
+        for edge in outgoing_edges {
+            let data = dfa.get_edge_data(&edge).unwrap();
+            let label = *data.borrow();
+
+            if ignore_case {
+                if label.to_lowercase().to_string() == c.to_lowercase().to_string()  {
+                    current_node = dfa.traverse(edge).unwrap();
+                    can_progress = true;
+                }
+            }
+            else {
+                if label == c {
+                    current_node = dfa.traverse(edge).unwrap();
+                    can_progress = true;
+                }
+            }
+        }
+
+        if !can_progress {
+            break;
+        }
+    }
+
+    let data = dfa.get_node_data(&current_node).unwrap();
+    let label = data.borrow();
+
+    return label.is_accepting();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn given_basic_input_when_searching_in_case_sensitive_mode_should_return_answers_and_respect_case() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents, false)
+        );
+    }
+
+    #[test]
+    fn given_basic_input_when_searching_in_case_insensitive_mode_should_return_answers_and_not_respect_case() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search(query, contents, true)
+        );
+    }
+
+    #[test]
+    fn given_test_input_when_searching_with_alternation_operator_should_correctly_return_answers() {
+        let query = "(safe)|(three)";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive.", "Pick three."],
+            search(query, contents, false)
+        );
+    }
+
+    #[test]
+    fn given_test_input_when_searching_with_star_operator_should_correctly_return_answers() {
+        let query = "thre*";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Pick three."],
+            search(query, contents, false)
+        );
+    }
+
+    #[test]
+    fn given_test_input_when_searching_with_plus_operator_should_correctly_return_answers() {
+        let query = ",+";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three,
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive.", "Pick three,"],
+            search(query, contents, false)
+        );
+    }
+
+    #[test]
+    fn given_test_input_when_searching_with_optional_operator_should_correctly_return_answers() {
+        let query = "e,?";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three,
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive.", "Pick three,", "Trust me."],
+            search(query, contents, false)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,18 @@
-use mygrep::{postfixer, nfa::build_nfa, dfa::build_dfa, regex::get_alphabet};
+use std::{env, process};
+
+use mygrep::Config;
 
 fn main() {
-    let postfix_regex = postfixer::transform("(a|b)a*".to_string()).unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    let alphabet = get_alphabet(&postfix_regex);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let (handle, nfa) = build_nfa(postfix_regex);
-
-    let (start, dfa) = build_dfa(handle, nfa, alphabet);
     
-    for source in 0..dfa.num_of_nodes() {
-        let outgoing_edges = dfa.outgoing_edges(source).unwrap();
-
-        print!("{}: ", source);
-
-        for edge in outgoing_edges {
-            let data = dfa.get_edge_data(&edge).unwrap();
-            let label: char = *data.borrow();
-            let target = dfa.traverse(edge).unwrap();
-
-            print!("{}-'{}'->{} ", source, label, target);
-        }
-        
-        println!();
+    if let Err(e) = mygrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
-
-    /*
-    for source in 0..nfa.num_of_nodes() {
-        let outgoing_edges = nfa.outgoing_edges(source).unwrap();
-        
-        if source == handle.get_start_state() {
-            print!("Start ")
-        }
-
-        if source == handle.get_accept_state() {
-            print!("Accepting ")
-        }
-
-        print!("{}: ", source);
-
-        for edge in outgoing_edges {
-            let data = nfa.get_edge_data(&edge).unwrap();
-            let label: Option<char> = data.borrow().get_label();
-            let target = nfa.traverse(edge).unwrap();
-
-            if let Some(c) = label {
-                print!("{}-'{}'->{} ", source, c, target);
-            }
-            else {
-                print!("{}->{} ", source, target);
-            }
-        }
-        
-        println!();
-    }
-    */
 }
